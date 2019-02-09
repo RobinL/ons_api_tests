19c30520a9a41ddee313ed063966efb8a426dac2
Robin Linacre <robin.linacre@digital.justice.gov.uk>
2019-02-09 17:55:57 +0000

    added web

---
+++ web1/js/notebook.js
@@ -0,0 +1,21 @@
+// URL: https://beta.observablehq.com/@robinl/hello-world
+// Title: Untitled
+// Author: Robin Linacre (@robinl)
+// Version: 136
+// Runtime version: 1
+
+const m0 = {
+  id: "019cc27370dc9c91@136",
+  variables: [
+    {
+
+    }
+  ]
+};
+
+const notebook = {
+  id: "019cc27370dc9c91@136",
+  modules: [m0]
+};
+
+export default notebook;
